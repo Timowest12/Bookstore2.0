e65cbb6f1a297efb128c8b77731755529b52c632
Timowest12 <timowester@gmail.com>
2021-12-14 23:39:06 +0100

    integrate api into redux

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,38 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import Book from './Book';
-import { addBook } from '../redux/books/books';
+import { addBook, getBooks } from '../redux/books/books';
 
 const BookList = () => {
   const dispatch = useDispatch();
-
   const submitForm = (e) => {
     e.preventDefault();
     const title = e.target.querySelector('#title');
     const author = e.target.querySelector('#author');
-    const newbook = { id: uuidv4(), title: title.value, author: author.value };
+    const newbook = {
+      item_id: uuidv4(),
+      title: title.value,
+      author: author.value,
+      category: 'thriller',
+    };
     title.value = '';
     author.value = '';
     dispatch(addBook(newbook));
+    console.log(newbook);
   };
+
   const bookList = useSelector((state) => state.booksReducer);
-  // console.log(bookList)
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
   return (
     <div>
-
       <h1>Awesome Books</h1>
-
       <ul>
-
         {bookList.map((book) => (
-
-          <Book key={book.id} book={book} />
+          <Book key={book.item_id} book={book} />
         ))}
       </ul>
       <form id="add-book-form" onSubmit={(e) => submitForm(e)}>
-
         <input id="title" placeholder="Title" />
         <input id="author" placeholder="Author" />
         <button type="submit">Add Book</button>
