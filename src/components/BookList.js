e074d4af8ac2608e2e40483a50f6d2001c5e8986
Timowest12 <timowester@gmail.com>
2021-12-14 22:37:40 +0100

    Add book wrapper

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import Book from './Book';
+import { addBook } from '../redux/books/books';
 
-const BookList = (props) => {
-  const { bookList } = props;
+const BookList = () => {
+  const dispatch = useDispatch();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const title = e.target.querySelector('#title');
+    const author = e.target.querySelector('#author');
+    const newbook = { id: uuidv4(), title: title.value, author: author.value };
+    title.value = '';
+    author.value = '';
+    dispatch(addBook(newbook));
+  };
+  const bookList = useSelector((state) => state.booksReducer);
+  // console.log(bookList)
+
   return (
     <div>
+
       <h1>Awesome Books</h1>
+
       <ul>
+
         {bookList.map((book) => (
+
           <Book key={book.id} book={book} />
         ))}
       </ul>
-      <form id="add-book">
-        <input placeholder="Book Title" />
-        <input placeholder="Book Author" />
+      <form id="add-book-form" onSubmit={(e) => submitForm(e)}>
+
+        <input id="title" placeholder="Title" />
+        <input id="author" placeholder="Author" />
         <button type="submit">Add Book</button>
       </form>
     </div>
